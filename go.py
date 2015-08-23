1001c6dcdf4c7fd6121929cfc00fb841cef34faf
Eric Beahan <ebeahan@gmail.com>
2015-08-22 22:55:09 -0500

    Documentation and refactoring

--- go.py
+++ go.py
@@ -1,4 +1,49 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+""" Title: go.py
+    Author: Eric Beahan
+
+    Stores a sqllite database of host entries that allows the user to connect
+    quickly using a menu-driven interface, destination hostname, or integer ID
+    values for each entry in the config db.
+
+    Sample usage:
+    > go.py
+    - Prints all entries in the current configdb and prompts user for an entry.
+
+    > go.py -m
+    - Prints the configuration entries and exits
+
+    > go.py -db
+    - Allows user to manually specify a configdb file to load. Default location
+     is ~/.go/.configdb
+
+    > go.py -d <hostname>
+    - User supplies a hostname of a device. Go automatically opens an SSH
+      connection to this host. The supplied hostname must already exist in
+      the config database.
+
+    > go.py -i <integer value>
+    - If the user knows the integer ID value for a config DB entry, he/she can
+      use that as a short-hand shortcut for opening a connection. For example,
+      if host.example.com is entry #3 in the config db:
+      > go.py -i 3
+
+    > go.py -c <config file>
+    - Allows a user to specify a CSV file with the following format to be
+      inserted into the the config DB in bulk:
+
+      # hostname, port, username, ipaddress
+
+    > go.py -e
+    - Places the user into DB entry mode where they can manually entry
+      additional host entries.
+
+    > go.py -r
+    - Places the user into a DB maintenance mode where they can manually remove
+      entries from the DB.
+
+"""
 
 import sys
 import socket
@@ -7,17 +52,27 @@ from subprocess import call
 import os
 import sqlite3
 
-class configDatabase:
 
-    def __init__(self, dbName=None):
-        if dbName == None:
+class ConfigDatabase(object):
+    """ Defines an instance of the GO configuration database
+    """
+
+    def __init__(self, dbname=None):
+        """ init
+        :param dbname: sqlite database
+        :return: n/a
+        """
+        if dbname is None:
             dbhome = '%s/.go' % (os.path.expanduser('~'))
-            self.dbname = '%s/.configdb' % (dbhome)
+            self.dbname = '%s/.configdb' % dbhome
         else:
-            self.dbname = dbName
+            self.dbname = dbname
         self.dbh = self._sqllite3_connect()
 
     def _sqllite3_connect(self):
+        """ Connects to sqlite database
+        :return: database handler for sqlite db
+        """
         dbh = sqlite3.connect(self.dbname)
         dbh.text_factory = str
         cur = dbh.cursor()
@@ -32,10 +87,18 @@ class configDatabase:
         dbh.commit()
         return dbh
 
-    def _insert_config_entries(self, hostname, port, username, ip):
-        if not hostname and port and username and ip:
+    def _insert_config_entries(self, hostname, port, username, ipaddress):
+        """ Insert entries into db
+
+        :param hostname: str
+        :param port: int
+        :param username: str
+        :param ip: str
+        :return: n/a
+        """
+        if not hostname and port and username and ipaddress:
             raise Exception('[*] Error: Missing required parameter')
-        print "Adding entry: ", hostname, port, username, ip
+        print "Adding entry: ", hostname, port, username, ipaddress
         cur = self.dbh.cursor()
         sql = """ \
         INSERT INTO config (
@@ -43,10 +106,9 @@ class configDatabase:
         )
         VALUES ( ?, ?, ?, ? )
         """
-
         try:
             params = [
-                hostname, port, username, ip
+                hostname, port, username, ipaddress
             ]
             cur.execute(sql, params)
         except:
@@ -54,6 +116,10 @@ class configDatabase:
         self.dbh.commit()
 
     def _remove_config_entires(self, hostname):
+        """ Remove entries from db
+        :param hostname: str
+        :return: n/a
+        """
         if not hostname:
             raise Exception('[*] Error: missing required parameter')
         print "Removing entry for", hostname
@@ -68,6 +134,10 @@ class configDatabase:
         self.dbh.commit()
 
     def _lookup_config_entries(self, hostname):
+        """ Lookup entries in db by hostname
+        :param hostname: str
+        :return: list of results
+        """
         if not hostname:
             raise Exception('[*] Error: missing required parameters')
         print "[*] Looking up entry for", hostname
@@ -81,55 +151,84 @@ class configDatabase:
             raise Exception('[*] Error: failed to complete SQL query')
         return cur.fetchall()
 
-    def _lookup_config_entries_by_id(self, id):
-        if id < 0 or id == 0:
+    def _lookup_config_entries_by_id(self, integer):
+        """ Lookup config entries by id
+        :param integer: int
+        :return: list of results
+        """
+        if integer < 0 or integer == 0:
             raise Exception('[*] Error: invalid ID number')
-        print "[*] Looking up ID #", id
+        print "[*] Looking up ID #", integer
         cur = self.dbh.cursor()
         sql = """\
         SELECT * FROM config WHERE id = ?
         """
         try:
-            cur.execute(sql, (id,))
+            cur.execute(sql, (integer,))
         except:
             raise Exception('[*] Error: failed to complete SQL query')
         return cur.fetchall()
 
-    def updateWithConfig(self, config):
-        with open(config, 'r') as f:
-            configData = f.read()
+    def update_with_config(self, config):
+        """ Bulk load from config csv file
+        :param config: str
+        :return: none
+        """
+        with open(config, 'r') as file:
+            config_data = file.read()
         recs = 0
         errs = 0
-        for line in configData.split('\n'):
+        for line in config_data.split('\n'):
             if not line:
                 continue
             try:
-                hostname, port, username, ip = line.split(',')
+                hostname, port, username, ipaddress = line.split(',')
                 # print hostname, port, username, ip
-                self._insert_config_entries(hostname, port, username, ip)
+                self._insert_config_entries(hostname, port, username, ipaddress)
                 recs += 1
             except:
                 errs += 1
                 continue
-        print '[*] Inserted %d records' % (recs)
+        print '[*] Inserted %d records' % recs
         if errs > 0:
-            raise Exception('[*] Errors on insert %d' % (errs))
+            raise Exception('[*] Errors on insert %d' % errs)
 
-    def updateNewEntry(self, hostname, port, username, ip):
+    def update_new_entry(self, hostname, port, username, ip):
+        """ Public method for updating new entries
+        :param hostname: str
+        :param port: int
+        :param username: str
+        :param ip: str
+        :return: none
+        """
         self._insert_config_entries(hostname, port, username, ip)
 
-    def removeEntry(self, hostname):
+    def remove_entry(self, hostname):
+        """ Public method for removing entries
+        :param hostname: str
+        :return: none
+        """
         self._remove_config_entires(hostname)
 
-    def lookupEntry(self, hostname):
+    def lookup_entry(self, hostname):
+        """ Public method for looking up entries
+        :param hostname: str
+        :return: list of results
+        """
         return self._lookup_config_entries(hostname)
 
-    def lookupId(self, id):
-        return self._lookup_config_entries_by_id(id)
+    def lookup_id(self, integer):
+        """ Public method for lookup up by ID
+        :param integer: int
+        :return: list of results
+        """
+        return self._lookup_config_entries_by_id(integer)
 
-    def printConfigDb(self):
+    def print_config_db(self):
+        """ Print the contents of the DB as a menu
+        :return: none
+        """
         cur = self.dbh.cursor()
-        i = 0 # Counter
         sql = """\
         SELECT * FROM config
         """
@@ -138,7 +237,11 @@ class configDatabase:
             print str(row[0]) + ") " + row[1]
         print "\n"
 
-def printWelcomeBanner():
+
+def print_welcome_banner():
+    """ Prints welcome banner
+    :return: none
+    """
     print ('\n'
            '\n'
            '############################################\n'
@@ -146,21 +249,28 @@ def printWelcomeBanner():
            ' Make a selection or use Ctrl+C to exit.\n'
            '############################################\n')
 
-def parseArgs():
+
+def parse_args():
     """Create the arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dest", dest="destination", help="Specify a destination host")
-    parser.add_argument("-e", "--entry", dest="entry", action="store_true", help="Enter DB entry mode")
-    parser.add_argument("-r", "--remove", dest="remove", action="store_true", help="Delete DB records")
-    parser.add_argument("-c", "--config", dest="configuration", help="Specify a configuration file")
-    parser.add_argument("-i", "--integer", dest="integer", help="Specify a destination host by it's integer")
-    parser.add_argument("-m", "--menu", dest="menu", action="store_true", help="Print the current configuration \
-                        and exit.")
-    parser.add_argument("-db", "--database", dest="database", help="Specify a configuration \
-                         sqllite3 database file.")
+    parser.add_argument("-d", "--dest", dest="destination",
+                        help="Specify a destination host")
+    parser.add_argument("-e", "--entry", dest="entry", action="store_true",
+                        help="Enter DB entry mode")
+    parser.add_argument("-r", "--remove", dest="remove", action="store_true",
+                        help="Delete DB records")
+    parser.add_argument("-c", "--config", dest="configuration",
+                        help="Specify a configuration file")
+    parser.add_argument("-i", "--integer", dest="integer",
+                        help="Specify a destination host by it's integer")
+    parser.add_argument("-m", "--menu", dest="menu", action="store_true",
+                        help="Print the current configuration and exit.")
+    parser.add_argument("-db", "--database", dest="database",
+                        help="Specify a configuration sqllite3 database file.")
     return parser.parse_args()
 
-def hostnameResolves(hostname):
+
+def hostname_resolves(hostname):
     """Checks to see if hostname is DNS resolvable"""
     try:
         socket.gethostbyname(hostname)
@@ -168,14 +278,14 @@ def hostnameResolves(hostname):
     except socket.error:
         return 0
 
-def openSSHConnection(currentHost):
+
+def open_ssh_connection(current_host):
     """Opens a SSH connection to currentHost"""
-    host = currentHost[1]
-    if not hostnameResolves(host):
-        host = currentHost[4]
-    
-    port = "-p" + str(currentHost[2])
-    username = "-l" + currentHost[3]
+    host = current_host[1]
+    if not hostname_resolves(host):
+        host = current_host[4]
+    port = "-p" + str(current_host[2])
+    username = "-l" + current_host[3]
 
     # make system call
     try:
@@ -183,103 +293,122 @@ def openSSHConnection(currentHost):
     except KeyboardInterrupt:
         sys.exit(0)
 
-def searchConfig(DB, dest):
+
+def search_config(configdb, dest):
+    """
+    :param db: ConfigDatabase class
+    :param dest: str format
+    :return: none
+    """
     i = 0
-    for item in DB:
+    for item in configdb:
         if dest in item:
-            return DB[i].split(",")
+            return configdb[i].split(",")
         else:
             i += 1
     print "Specified host not listed in the configuration"
     print "The program will now exit gracefully"
     sys.exit(0)
 
-def entryMode(configdb):
+
+def entry_mode(configdb):
+    """
+    :param configdb: ConfigDatabase class
+    :return: none
+    """
     repeat = True
     print "Entering entry mode...."
-    print "Please enter a hostname, port, username, and IP address for each entry."
+    print "Please enter a hostname, port, username, and IP address for \
+           each entry."
     while repeat:
         hostname = raw_input("Hostname? ")
         port = raw_input("Port? ")
         username = raw_input("Username? ")
         ipaddress = raw_input("IP address? ")
         print "Adding entry ", hostname, port, username, ipaddress
-        configdb.updateNewEntry(hostname, port, username, ipaddress)
-        repeatPrompt = raw_input("Continue? [Y/n] ")
-        if repeatPrompt != "Y":
+        configdb.update_new_entry(hostname, port, username, ipaddress)
+        repeat_prompt = raw_input("Continue? [Y/n] ")
+        if repeat_prompt != "Y":
             repeat = False
 
-def removeMode(configdb):
+
+def remove_mode(configdb):
+    """
+    :param configdb: ConfigDatabase class
+    :return: none
+    """
     repeat = True
     print "Entering removal mode..."
     print "Please enter the hostname of the record you want to delete."
     while repeat:
         hostname = raw_input("> ")
-        configdb.removeEntry(hostname)
-        repeatPrompt = raw_input("Continue [Y/n] ")
-        if repeatPrompt != "Y":
+        configdb.remove_entry(hostname)
+        repeat_prompt = raw_input("Continue [Y/n] ")
+        if repeat_prompt != "Y":
             repeat = False
 
+
 def main(args):
+    """ Main function call
+    :param args: ParseArgs() object
+    :return: None
+    """
 
-    if args.database == None:
-        sqlDB = configDatabase()
+    if args.database is None:
+        sqldb = ConfigDatabase()
     else:
-        sqlDB = configDatabase(args.database)
-
-    # for testing
-    #host = raw_input("hostname> ")
-    #configLookup(sqlDB, host)
+        sqldb = ConfigDatabase(args.database)
 
     if args.entry:
-        entryMode(sqlDB)
+        entry_mode(sqldb)
 
     if args.remove:
-        removeMode(sqlDB)
+        remove_mode(sqldb)
 
-    if args.configuration != None:
-        txtConfig = args.configuration
-        sqlDB.updateWithConfig(txtConfig)
+    if args.configuration is not None:
+        txt_config = args.configuration
+        sqldb.update_with_config(txt_config)
         sys.exit(0)
 
     if args.menu:
-        printWelcomeBanner()
-        sqlDB.printConfigDb()
+        print_welcome_banner()
+        sqldb.print_config_db()
         sys.exit(0)
 
     # Print Destination
-    if args.destination != None:
-      entry = sqlDB.lookupEntry(args.destination)[0]
-      print "[*] Attempting to connect to", entry[1], "on port", str(entry[2]), "using username", entry[3], "..."
-      openSSHConnection(entry)
-      sys.exit(0)
+    if args.destination is not None:
+        entry = sqldb.lookup_entry(args.destination)[0]
+        print "[*] Attempting to connect to", entry[1], "on port", \
+               str(entry[2]), "using username", entry[3], "..."
+        open_ssh_connection(entry)
+        sys.exit(0)
 
     # Connect to host based on config integer value
-    if args.integer != None:
-        entry = sqlDB.lookupId(args.integer)[0]
-        print "[*] Attempting to connect to", entry[1], "on port", str(entry[2]), "using username", entry[3], "..."
-        openSSHConnection(entry)
+    if args.integer is not None:
+        entry = sqldb.lookup_id(args.integer)[0]
+        print "[*] Attempting to connect to", entry[1], "on port", \
+               str(entry[2]), "using username", entry[3], "..."
+        open_ssh_connection(entry)
         sys.exit(0)
 
     # print the welcome banner
-    printWelcomeBanner()
+    print_welcome_banner()
 
     # print interactive menu
-    sqlDB.printConfigDb()
+    sqldb.print_config_db()
 
     # User enters selection. -1 to adjust for index 0
     selection = raw_input("Please enter your selection: ")
 
-
     # Open SSH connection and pass user selection
-    entry = sqlDB.lookupId(selection)[0]
-    print "[*] Attempting to connect to", entry[1], "on port", str(entry[2]), "using username", entry[3], "..."
-    openSSHConnection(entry)
+    entry = sqldb.lookup_id(selection)[0]
+    print "[*] Attempting to connect to", entry[1], "on port", \
+           str(entry[2]), "using username", entry[3], "..."
+    open_ssh_connection(entry)
     sys.exit(0)
-    
 
 if __name__ == "__main__":
     try:
-        main(parseArgs())
+        main(parse_args())
     except KeyboardInterrupt:
         sys.exit(0)
